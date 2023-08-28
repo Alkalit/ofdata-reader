426cc73e1ff8fa79046defc3b3409bdd1d11d6b0
alkalit <lyoffa412@gmail.com>
2023-08-28 21:30:48 +0500

    Temporally removing multiprocessing

--- src/project/application/services.py
+++ src/project/application/services.py
@@ -1,5 +1,4 @@
 import json
-from multiprocessing.pool import Pool, AsyncResult
 
 from src.project.adapters.ofdata import download_file
 from src.project.adapters.zipfile import yield_data, Filepath
@@ -11,11 +10,5 @@ def do_service(filepath: Filepath | None = None) -> None:
     else:
         downloaded_file_path = filepath
 
-    with Pool(processes=4) as pool:
-        fs: list[AsyncResult] = []
         for json_data in yield_data(filepath=downloaded_file_path):
-            future = pool.apply_async(json.loads, json_data)
-            fs.append(future)
-
-        for result in fs:
-            result.wait()
+            json.loads(json_data)
