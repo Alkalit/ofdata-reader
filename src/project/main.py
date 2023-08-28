858f5d78c3bfe704f32a5aec87d4bd16794a4317
alkalit <lyoffa412@gmail.com>
2023-08-28 17:55:14 +0500

    Removing backends

--- src/project/main.py
+++ src/project/main.py
@@ -1,58 +1,16 @@
 import requests
 import zipfile
 import json
-import orjson
 import timeit
 from multiprocessing.pool import Pool, AsyncResult
 from typing import Any, Generator
-from abc import ABC
 
 # resp = requests.get("https://ofdata.ru/open-data/download/egrul.json.zip")
 serialized_json = Any
 
 
-class BaseJsonBackend(ABC):
-
-    def loads(self, data: bytes | bytearray | str | memoryview, **options) -> Any:
-        ...
-
-    def dumps(self, obj: Any, **options) -> str:
-        """
-        obj - A python object to be serialized.
-        options - options for the specific backend
-        """
-        ...
-
-
-class BuiltinJsonBackend(BaseJsonBackend):
-
-    def __init__(self):
-        self._lib = json
-
-    def loads(self, data: bytes | bytearray | str | memoryview, **options) -> Any:
-        return self._lib.loads(data, **options)
-
-    def dumps(self, obj: Any, **options) -> str:
-        return self._lib.dumps(obj, **options)
-
-
-class OrjsonBackend(BaseJsonBackend):
-
-    def __init__(self):
-        self._lib = orjson
-
-    def loads(self, data: bytes | bytearray | str | memoryview, **options) -> Any:
-        return self._lib.loads(data)
-
-    def dumps(self, obj: Any, **options) -> str:
-        return self._lib.dumps(
-            obj,
-            option=orjson.OPT_INDENT_2,
-            **options,
-        ).decode('utf-8')
-
-
-# Is used for performance testing
+# Is used for performance testing to separated IO operations
+# from CPU-bound
 def get_data(chunk: int = 100) -> list[serialized_json]:
     result = []
     with zipfile.ZipFile('../../egrul.json.zip') as archive:
@@ -73,7 +31,7 @@ def main() -> None:
     with Pool(processes=4) as pool:
         fs: list[AsyncResult] = []
         for json_data in yield_data(1000):
-            future = pool.apply_async(orjson.loads, json_data)
+            future = pool.apply_async(json.loads, json_data)
             fs.append(future)
 
         for result in fs:
