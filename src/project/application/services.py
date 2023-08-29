c7aa533d5adeff91ea400359c8b23985d4a4b3d5
alkalit <lyoffa412@gmail.com>
2023-08-29 16:55:26 +0500

    Adding "domain" layer

--- src/project/application/services.py
+++ src/project/application/services.py
@@ -2,6 +2,10 @@ import json
 
 from src.project.adapters.ofdata import download_file
 from src.project.adapters.zipfile import yield_data, Filepath
+from src.project.domain.models import Entry, Svokved, Svadresul, Adresrf, Svokvedosn
+
+
+KHABAROVSK_KRAI = "27"
 
 
 def do_service(filepath: Filepath | None = None) -> None:
@@ -10,27 +14,28 @@ def do_service(filepath: Filepath | None = None) -> None:
     else:
         downloaded_file_path = filepath
 
-        for idx, json_data in enumerate(yield_data(filepath=downloaded_file_path, chunk=100)):
-            data = json.loads(json_data)
+        for idx, json_data in enumerate(yield_data(filepath=downloaded_file_path, chunk=10)):
+            data: list[Entry] = json.loads(json_data)
             for entry in data:
                 kodokved: str | None = None
                 adresrf: dict | None = None
+
                 name: str = entry["name"]
                 inn: str | None = entry["data"].get("ИНН")
                 kpp: str | None = entry["data"].get("КПП")
-                svokded: dict | None = entry["data"].get("СвОКВЭД")
-                svadresul: dict | None = entry["data"].get("СвАдресЮЛ")
+                svokved: Svokved | None = entry["data"].get("СвОКВЭД")
+                svadresul: Svadresul | None = entry["data"].get("СвАдресЮЛ")
 
                 if not svadresul:
                     continue
                 else:
-                    adresrf = svadresul.get("АдресРФ")
-                    if not adresrf or (adresrf["КодРегион"] != "27"):
+                    adresrf: Adresrf | None = svadresul.get("АдресРФ")
+                    if not adresrf or (adresrf["КодРегион"] != KHABAROVSK_KRAI):
                         continue
 
-                if svokded:
-                    svokvedosn: str | dict = svokded.get("СвОКВЭДОсн")
+                if svokved:
+                    svokvedosn: Svokvedosn | None = svokved.get("СвОКВЭДОсн")
                     if svokvedosn:
-                        kodokved = svokvedosn.get("КодОКВЭД")
+                        kodokved: str = svokvedosn.get("КодОКВЭД")
 
                 print(name, inn, kpp, kodokved, adresrf)
---
+++ src/project/domain/models.py
@@ -0,0 +1,74 @@
+from typing import TypedDict, Any
+
+Numeric = str
+Date = str
+
+
+class NaselPunkt(TypedDict):
+    ТипНаселПункт: str
+    НаимНаселПункт: str
+
+
+class Region(TypedDict):
+    ТипРегион: str
+    НаимРегион: str
+
+
+class Gorod(TypedDict):
+    ТипГород: str
+    НаимГород: str
+
+
+class Uliza(TypedDict):
+    ТипУлица: str
+    НаимУлица: str
+
+
+class Rayon(TypedDict):
+    ТипРайон: str
+    НаимРайон: str
+
+
+class Adresrf(TypedDict):
+    Индекс: Numeric | None
+    КодРегион: Numeric
+    Регион: Region
+    Дом: str | None
+    Корпус: str | None
+    Кварт: str | None
+    Район: Rayon | None
+    Город: Gorod | None
+    НаселПункт: NaselPunkt | None
+    Улица: Uliza | None
+
+
+class Svadresul(TypedDict):
+    АдресРФ: Adresrf | None
+
+
+class Svokvedosn(TypedDict):
+    КодОКВЭД: str | None
+
+
+class Svokved(TypedDict):
+    СвОКВЭДОсн: Svokvedosn | None
+
+
+class Data(TypedDict):
+    ИНН: Numeric | None
+    КПП: Numeric | None
+    ОГРН: Numeric
+    КодОПФ: Numeric
+    СпрОПФ: str
+    ДатаВып: Date
+    СвОКВЭД: Svokved | None
+    СвАдресЮЛ: Svadresul | None
+
+
+class Entry(TypedDict):
+    ogrn: Numeric
+    inn: Numeric
+    kpp: Numeric
+    name: str
+    full_name: str
+    data: Data
