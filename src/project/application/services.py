596780d234af28351c1d054e3f777080963ee751
alkalit <lyoffa412@gmail.com>
2023-08-29 19:24:34 +0500

    Filtration is done

--- src/project/application/services.py
+++ src/project/application/services.py
@@ -1,12 +1,13 @@
 import json
+import orjson as json  # TODO for testing. Rollback later
 
 from src.project.adapters.ofdata import download_file
 from src.project.adapters.zipfile import yield_data, Filepath
-from src.project.domain.models import Entry, Svokved, Svadresul, Adresrf, Svokvedosn
-
+from src.project.domain.models import Entry, Svokved, Svadresul, Adresrf, Svokvedosn, Gorod
 
 KHABAROVSK_KRAI = "27"
 OKVED_PREFIX = "62."
+CITY_NAME = "ХАБАРОВСК"
 
 
 def do_service(filepath: Filepath | None = None) -> None:
@@ -15,28 +16,41 @@ def do_service(filepath: Filepath | None = None) -> None:
     else:
         downloaded_file_path = filepath
 
-    for json_data in yield_data(filepath=downloaded_file_path, chunk=10):
+    for json_data in yield_data(filepath=downloaded_file_path, chunk=1000):
         data: list[Entry] = json.loads(json_data)
         for entry in data:
             kodokved: str | None = None
             adresrf: Adresrf | None = None
 
             name: str = entry["name"]
-            inn: str | None = entry["data"].get("ИНН")
-            kpp: str | None = entry["data"].get("КПП")
-            svokved: Svokved | None = entry["data"].get("СвОКВЭД")
+            inn: str | None = entry["inn"]
+            kpp: str | None = entry["kpp"]
             svadresul: Svadresul | None = entry["data"].get("СвАдресЮЛ")
+            svokved: Svokved | None = entry["data"].get("СвОКВЭД")
 
-            if not svadresul:
+            if svadresul is None:
                 continue
             else:
                 adresrf: Adresrf | None = svadresul.get("АдресРФ")
-                if not adresrf or (adresrf["КодРегион"] != KHABAROVSK_KRAI):
+                if (adresrf is None) or (adresrf["КодРегион"] != KHABAROVSK_KRAI):  # TODO check with str.lower
                     continue
+                else:
+                    gorod: Gorod = adresrf.get("Город")
+                    if gorod is None:
+                        continue
+                    naimgorod = gorod.get("НаимГород")
+                    if (naimgorod is None) or (naimgorod != CITY_NAME):
+                        continue
 
-            if svokved:
+            if not svokved:
+                continue
+            else:
                 svokvedosn: Svokvedosn | None = svokved.get("СвОКВЭДОсн")
-                if svokvedosn:
+                if not svokvedosn:
+                    continue
+                else:
                     kodokved: str = svokvedosn.get("КодОКВЭД")
+                    if not kodokved.startswith(OKVED_PREFIX):
+                        continue
 
             print(name, inn, kpp, kodokved, adresrf)
