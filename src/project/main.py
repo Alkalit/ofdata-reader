162ff73fde92a8dbc329cfc8efa15204ced722f9
alkalit <lyoffa412@gmail.com>
2023-08-30 20:25:24 +0500

    Successful init log

--- src/project/main.py
+++ src/project/main.py
@@ -1,9 +1,13 @@
 import timeit
+import logging
 
 from project.common.argparser import setup_argparser
 from project.common.logging import setup_logging
 from project.application.services import do_service
 from project.adapters.db import get_connection, create_table
+
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
@@ -13,6 +17,7 @@ def main() -> None:
 
     connection = get_connection()
     create_table(connection)
+    logger.info("Successfully initialized application.")
 
     do_service(connection, filepath=args.file)
 
