352b49712c6c90520f2dabd31684e70c7bf05751
alkalit <lyoffa412@gmail.com>
2023-08-30 18:08:15 +0500

    Moving argparser configuration function into a separate module

---
+++ src/project/common/argparser.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def configure_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        "rostelekom downloader",
+        description="Downloads sample dataset of russian legal entities from ofdata.ru"
+    )
+    parser.add_argument("--file", metavar="-f", nargs="?",
+                        help="Process data from the specified file rather than downloading it")
+
+    return parser
--- src/project/main.py
+++ src/project/main.py
@@ -1,20 +1,9 @@
-import argparse
 import timeit
 import sqlite3
 
+from project.common.argparser import configure_argparser
 from project.common.logging import setup_logging
 from src.project.application.services import do_service
-
-
-def configure_argparser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        "rostelekom downloader",
-        description="Downloads sample dataset of russian legal entities from ofdata.ru"
-    )
-    parser.add_argument("--file", metavar="-f", nargs="?",
-                        help="Process data from the specified file rather than downloading it")
-
-    return parser
 
 
 def main() -> None:
