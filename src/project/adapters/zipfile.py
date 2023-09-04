e22509a53e29929a6a085ebe36b24ff525041232
alkalit <lyoffa412@gmail.com>
2023-09-04 16:14:28 +0500

    Using buffered IO

--- src/project/adapters/zipfile.py
+++ src/project/adapters/zipfile.py
@@ -1,3 +1,4 @@
+from io import BufferedReader
 import zipfile
 from typing import Generator, Any
 
@@ -20,13 +21,15 @@ def yield_data(filepath: Filepath, chunk: int | None = None) -> Generator[str, N
     """
     Yields a stream of individual files from archive in filepath
     """
-    with zipfile.ZipFile(filepath) as archive:
-        if chunk is None:
-            num_of_files = archive.namelist()
-        else:
-            num_of_files = archive.namelist()[:chunk]
+    buffer_size = 10 * 1024 * 1024
+    with open(filepath, 'rb') as file:
+        with zipfile.ZipFile(BufferedReader(file, buffer_size=buffer_size)) as archive:
+            if chunk is None:
+                num_of_files = archive.namelist()
+            else:
+                num_of_files = archive.namelist()[:chunk]
 
-        yield len(num_of_files)
-        for sample in num_of_files:
-            with archive.open(sample) as jsondata:
-                yield jsondata.read()
+            yield len(num_of_files)
+            for sample in num_of_files:
+                with archive.open(sample) as jsondata:
+                    yield jsondata.read()
